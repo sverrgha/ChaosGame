e2f4d9ecbaf4ff9b86059f4a70bee060fc52969a
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2024-02-19 15:40:59 +0100

    Feat: Implement AffineTransform2D class for 2D affine transformations.

---
+++ src/main/java/edu/ntnu/idatt2003/model/AffineTransform2D.java
@@ -0,0 +1,49 @@
+package edu.ntnu.idatt2003.model;
+
+/**
+ * Represents a 2-dimensional affine transformation.
+ * Contains a constructor and a method for transforming a 2-dimensional vector.
+ * Goal: act as a model for a 2-dimensional affine transformation.
+ * autor: nicklapt
+ */
+
+public class AffineTransform2D implements Transform2D {
+
+  /**
+   * The 2x2 matrix of the affine transformation.
+   */
+
+  private Matrix2x2 matrix;
+
+  /**
+   * The 2-dimensional vector of the affine transformation.
+   */
+  private Vector2d vector;
+
+  /**
+   * Constructs a new AffineTransform2D with specified values for its matrix and vector.
+   *
+   * @param matrix The 2x2 matrix of the affine transformation.
+   * @param vector The 2-dimensional vector of the affine transformation.
+   */
+
+  public AffineTransform2D(Matrix2x2 matrix, Vector2d vector) {
+    this.matrix = matrix;
+    this.vector = vector;
+  }
+
+  /**
+   * Transforms a 2-dimensional vector using the affine transformation.
+   *
+   * @param point The 2-dimensional vector to be transformed.
+   * @return A new Vector2D representing the result of the transformation.
+   */
+
+  @Override
+  public Vector2d transform(Vector2d point) {
+    return matrix.multiply(point).add(vector);
+  }
+
+
+
+}
