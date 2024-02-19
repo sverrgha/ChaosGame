a60298310fd6fd7871b17fa9ada061cede668d09
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2024-02-19 14:42:40 +0100

    feat: implement Transform2D interface for 2D affine and JuliaTransformations

---
+++ src/main/java/edu/ntnu/idatt2003/model/Transform2D.java
@@ -0,0 +1,12 @@
+package edu.ntnu.idatt2003.model;
+
+/**
+ * Interface for transforming 2D points.
+ * autor: nicklapt
+ */
+
+public interface Transform2D {
+
+  Vector2d transform(Vector2d point);
+
+}
