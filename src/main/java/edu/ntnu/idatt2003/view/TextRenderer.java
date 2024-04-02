505c00e6fdd4353410a7c7a42464fd18bd0250e8
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2024-04-02 20:31:25 +0200

    Feat: Implement CLI-application. This includes methods for user to read, write, decide iterations and show canvas.

---
+++ src/main/java/edu/ntnu/idatt2003/view/TextRenderer.java
@@ -0,0 +1,37 @@
+package edu.ntnu.idatt2003.view;
+
+/**
+ * Represents a text renderer.
+ * Contains constants for menu options and methods for showing the menu, entering a path and entering steps.
+ * Goal: act as a view for a text renderer.
+ *
+ */
+
+public class TextRenderer {
+
+  public static final String READ_FILE = "1";
+  public static final String WRITE_FILE = "2";
+  public static final String RUN_ITERATIONS = "3";
+  public static final String SHOW_CANVAS = "4";
+  public static final String EXIT = "5";
+
+  public void showMenu() {
+    System.out.println("Menu:");
+    System.out.println("1. Read description from file");
+    System.out.println("2. Write description to file");
+    System.out.println("3. Run ChaosGame a given number of steps");
+    System.out.println("4. Show Canvas");
+    System.out.println("5. Exit");
+  }
+
+  public void enterPath() {
+    System.out.println("Enter path to file:");
+  }
+
+  public void enterSteps() {
+    System.out.println("Enter number of steps:");
+  }
+
+
+
+}
