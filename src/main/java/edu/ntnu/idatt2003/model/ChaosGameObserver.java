0d9d35a25caef8d51e33f82f3baba795cd740a28
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-04-18 11:35:49 +0200

    Feat: Implement ChaosGameObserver interface

---
+++ src/main/java/edu/ntnu/idatt2003/model/ChaosGameObserver.java
@@ -0,0 +1,15 @@
+package edu.ntnu.idatt2003.model;
+
+/**
+ * Represents a ChaosGameObserver.
+ * Contains a method for updating the observer.
+ * Goal: act as an observer for a chaos game.
+ *
+ */
+public interface ChaosGameObserver {
+
+  /**
+   * Updates the observer.
+   */
+  void update();
+}
