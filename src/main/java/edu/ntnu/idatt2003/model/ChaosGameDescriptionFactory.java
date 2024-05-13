79fe6c1dfc04f2a9e96524dcc13198f998e56ed2
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2024-05-13 17:34:52 +0200

    Refactor: improve code readability within ChaosGameDescriptionFactory.

--- src/main/java/edu/ntnu/idatt2003/model/ChaosGameDescriptionFactory.java
+++ src/main/java/edu/ntnu/idatt2003/model/ChaosGameDescriptionFactory.java
@@ -1,6 +1,6 @@
 package edu.ntnu.idatt2003.model;
 
-import edu.ntnu.idatt2003.enums.descriptionTypeEnum;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +20,13 @@ public class ChaosGameDescriptionFactory {
     return switch (descriptionType) {
       case SIERPINSKI_TRIANGLE -> sierpinskiTriangle();
       case BARNSLEY_FERN -> barnsleyFern();
+      case JULIA -> juliaTransformation();
     };
+  }
+  public enum descriptionTypeEnum {
+    SIERPINSKI_TRIANGLE,
+    BARNSLEY_FERN,
+    JULIA,
   }
 
   /**
@@ -30,20 +36,7 @@ public class ChaosGameDescriptionFactory {
    */
 
   private static ChaosGameDescription sierpinskiTriangle() {
-    Vector2d min = new Vector2d(0, 0);
-    Vector2d max = new Vector2d(1, 1);
-    Transform2D sierpinski1 = new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
-        new Vector2d(0, 0));
-    Transform2D sierpinski2 = new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
-        new Vector2d(0.5, 0));
-    Transform2D sierpinski3 = new AffineTransform2D(new Matrix2x2(0.5, 0, 0, 0.5),
-        new Vector2d(0.25, 0.5));
-    List<Transform2D> transform2DList = new ArrayList<>();
-    transform2DList.add(sierpinski1);
-    transform2DList.add(sierpinski2);
-    transform2DList.add(sierpinski3);
-    ChaosGameDescription description = new ChaosGameDescription(min, max, transform2DList);
-    return description;
+    return transformations("src/test/resources/Affine2DExample.txt");
   }
 
   /**
@@ -53,25 +46,28 @@ public class ChaosGameDescriptionFactory {
    */
 
   private static ChaosGameDescription barnsleyFern() {
-    Vector2d min = new Vector2d(-2.5, 0);
-    Vector2d max = new Vector2d(2.5, 10);
-
-    Transform2D fern1 = new AffineTransform2D(new Matrix2x2(0, 0, 0, 0.16), new Vector2d(0, 0));
-    Transform2D fern2 = new AffineTransform2D(new Matrix2x2(0.85, 0.04, -0.04, 0.85),
-        new Vector2d(0, 1.6));
-    Transform2D fern3 = new AffineTransform2D(new Matrix2x2(0.2, -0.26, 0.23, 0.22),
-        new Vector2d(0, 1.6));
-    Transform2D fern4 = new AffineTransform2D(new Matrix2x2(-0.15, 0.28, 0.26, 0.24),
-        new Vector2d(0, 0.44));
-
-    List<Transform2D> transform2DList = new ArrayList<>();
-    transform2DList.add(fern1);
-    transform2DList.add(fern2);
-    transform2DList.add(fern3);
-    transform2DList.add(fern4);
-
-    ChaosGameDescription description = new ChaosGameDescription(min, max, transform2DList);
-    return description;
-
+    return transformations("src/test/resources/BarnsleyFern.txt");
   }
+
+  /**
+   * A static method for the julia transformation.
+   *
+   * @return a description based on a julia transformation.
+   */
+
+  private static ChaosGameDescription juliaTransformation() {
+    return transformations("src/test/resources/JuliaExample.txt");
+  }
+
+  private static ChaosGameDescription transformations(String pathToFile) {
+    try {
+      return new ChaosGameFileHandler().readFromFile(pathToFile);
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException("File " + pathToFile + " not found." + e.getMessage());
+    }
+  }
+
+
 }
+
+
