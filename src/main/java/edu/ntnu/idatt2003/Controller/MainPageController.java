8ea260ef5ebdec6935fdf5d3ba69dafe09547e2d
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-05-15 16:08:33 +0200

    Feat: Add possibility to upload a custom text file

--- src/main/java/edu/ntnu/idatt2003/Controller/MainPageController.java
+++ src/main/java/edu/ntnu/idatt2003/Controller/MainPageController.java
@@ -2,7 +2,16 @@ package edu.ntnu.idatt2003.controller;
 
 import edu.ntnu.idatt2003.model.ChaosGame;
 import edu.ntnu.idatt2003.model.ChaosGameDescriptionFactory;
+import edu.ntnu.idatt2003.model.ChaosGameFileHandler;
 import edu.ntnu.idatt2003.view.MainPageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.InputMismatchException;
 
 /**
  * The controller class for the main page of the ChaosGame application.
@@ -12,6 +21,8 @@ import edu.ntnu.idatt2003.view.MainPageView;
 public class MainPageController {
   private ChaosGame game;
   private final MainPageView view;
+
+  private static final String TRANSFORMATIONS_PATH = "src/main/resources/transformations/";
 
   /**
    * The constructor for the MainPageController class.
@@ -48,5 +59,51 @@ public class MainPageController {
     this.game = new ChaosGame(ChaosGameDescriptionFactory
             .get(descriptionType), 600, 600);
     view.render(game.getCanvas());
+  }
+
+  /**
+   * Validates the file that is uploaded., and calls storeFile if the
+   * file exists and is formatted correctly.
+   *
+   * @param file The file to upload.
+   * @throws InputMismatchException If the file is not found or the input is invalid.
+   */
+  public void uploadFile(File file) {
+    try {
+      validateFile(file);
+      if (!Files.exists(Path.of(TRANSFORMATIONS_PATH + file.getName()))
+              || view.askConfirmation("File already exists. Do you want to overwrite it?")) {
+        storeFile(file);
+      }
+    } catch (Exception e) {
+      view.showAlert(e.getMessage());
+    }
+  }
+
+  private void validateFile(File file)
+          throws InputMismatchException, FileNotFoundException {
+    try {
+      new ChaosGameFileHandler().readFromFile(file);
+    } catch (InputMismatchException e) {
+      throw new InputMismatchException(e.getMessage());
+    } catch (FileNotFoundException e) {
+      throw new FileNotFoundException(e.getMessage());
+    }
+  }
+
+  /**
+   * Stores the file in the resources/transformations folder of the project.
+   *
+   * @param file The file to store.
+   */
+  private void storeFile(File file) {
+    try {
+      String projectPath = System.getProperty("user.dir");
+      String destinationPath = projectPath + File.separator
+              + TRANSFORMATIONS_PATH + file.getName();
+      Files.copy(file.toPath(), Path.of(destinationPath), StandardCopyOption.REPLACE_EXISTING);
+    } catch (IOException e) {
+      throw new InputMismatchException("Error copying file: " + e.getMessage());
+    }
   }
 }
--- src/main/java/edu/ntnu/idatt2003/view/MainPageView.java
+++ src/main/java/edu/ntnu/idatt2003/view/MainPageView.java
@@ -4,11 +4,14 @@ import edu.ntnu.idatt2003.controller.MainPageController;
 import edu.ntnu.idatt2003.model.ChaosCanvas;
 import edu.ntnu.idatt2003.model.ChaosGameDescriptionFactory;
 import edu.ntnu.idatt2003.utils.Sizes;
+
+import java.io.File;
 import java.util.Objects;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -16,6 +19,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * The MainPageView class is the main page of the application.
@@ -156,15 +161,46 @@ public class MainPageView extends Scene {
   }
 
   /**
+   * Opens a file chooser dialog that enables the user to upload a custom
+   * text file with a chaos game description. The file is then uploaded
+   * by the controller. If an exception occurs, an alert is shown.
+   */
+  private void uploadFile() {
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.getExtensionFilters().add(
+            new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+
+    Stage fileChooserStage = new Stage();
+    File file = fileChooser.showOpenDialog(fileChooserStage);
+    if (file != null) {
+      controller.uploadFile(file);
+    }
+  }
+
+  /**
    * Shows an alert with a specified message to give feedback to the user.
    *
    * @param message The message to be shown in the alert.
    */
-  private void showAlert(String message) {
+  public void showAlert(String message) {
     Alert alert = new Alert(Alert.AlertType.WARNING);
     alert.setTitle("Error");
     alert.setHeaderText(null);
     alert.setContentText(message);
     alert.showAndWait();
+  }
+
+  /**
+   * Asks the user for confirmation with a specified message, to ensure
+   * that the user wants to perform the specific action.
+   *
+   * @param message The message to be shown in the confirmation dialog.
+   * @return True if the user confirms, false otherwise.
+   */
+  public boolean askConfirmation(String message) {
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION, message, ButtonType.YES, ButtonType.NO);
+    alert.setHeaderText(null);
+    alert.showAndWait();
+    return alert.getResult() == ButtonType.YES;
   }
 }
