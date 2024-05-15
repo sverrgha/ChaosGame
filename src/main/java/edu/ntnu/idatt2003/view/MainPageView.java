fa4bb7d162f83491f1ecb4fe723660d70eea088e
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2024-05-15 12:42:38 +0200

    Feat: Implement code in the view layer for adding a new transformation.

--- src/main/java/edu/ntnu/idatt2003/view/MainPageView.java
+++ src/main/java/edu/ntnu/idatt2003/view/MainPageView.java
@@ -5,17 +5,21 @@ import edu.ntnu.idatt2003.model.ChaosCanvas;
 import edu.ntnu.idatt2003.model.ChaosGameDescriptionFactory;
 import edu.ntnu.idatt2003.utils.Sizes;
 import java.util.Objects;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 /**
  * The MainPageView class is the main page of the application.
@@ -60,11 +64,16 @@ public class MainPageView extends Scene {
    * The button container contains 7 buttons.
    */
   private void createPageContainer(ChaosCanvas chaosCanvas) {
-    pageContainer.getChildren().add(new ImageView(
-            ChaosImage.createImageFromCanvas(chaosCanvas)));
+    ImageView imageView = new ImageView(ChaosImage.createImageFromCanvas(chaosCanvas));
 
-    HBox buttonContainer = createButtonContainer();
-    pageContainer.getChildren().add(buttonContainer);
+    HBox mainContent = new HBox();
+    mainContent.setSpacing(20);
+    mainContent.setAlignment(Pos.CENTER);
+    mainContent.getChildren().addAll(imageView, createTransformationPanel());
+
+
+    pageContainer.getChildren().add(mainContent);
+    pageContainer.getChildren().add(createButtonContainer());
     pageContainer.getStyleClass().add("page-container");
     pageContainer.setMaxHeight(Sizes.SCREEN_HEIGHT - 50);
     pageContainer.setMaxWidth(Sizes.SCREEN_WIDTH - 50);
@@ -78,6 +87,7 @@ public class MainPageView extends Scene {
    *
    * @return The button container.
    */
+
   private HBox createButtonContainer() {
     HBox buttonContainer = new HBox();
     buttonContainer.getStyleClass().add("button-container");
@@ -134,10 +144,147 @@ public class MainPageView extends Scene {
   }
 
   /**
-   * Creates an input field for custom number of steps.
+   * Creates the editing panel for adding new transformations. It uses combobox to give the
+   * alternative transformation type, buttons to save, cancel and add to file button. And
+   * text fields to give start vector, input vector, and the necessary matrix.
    *
-   * @return The input field.
+   * @return The editing panel.
    */
+  public VBox createTransformationPanel() {
+    VBox addPanel = createMainPanel();
+    ComboBox<TransformationType> transformationComboBox = createTransformationComboBox();
+    HBox transformationInputField = createTransformationInputField(transformationComboBox);
+
+    TextField startVectorField = createTextField("Enter start vector");
+    TextField endVectorField = createTextField("Enter end vector");
+
+    Button saveButton = createSaveButton(transformationComboBox, transformationInputField, startVectorField, endVectorField);
+    Button cancelButton = createCancelButton(transformationInputField, startVectorField, endVectorField);
+    Button addFileButton = createAddFileButton();
+
+    addPanel.getChildren().addAll(transformationComboBox, transformationInputField, startVectorField, endVectorField, saveButton, cancelButton, addFileButton);
+    StackPane.setAlignment(addPanel, Pos.BOTTOM_LEFT);
+
+    return addPanel;
+  }
+
+
+  private VBox createMainPanel() {
+    VBox addPanel = new VBox(10);
+    addPanel.getStyleClass().add("add-panel");
+    addPanel.setAlignment(Pos.CENTER);
+    return addPanel;
+  }
+
+  private ComboBox<TransformationType> createTransformationComboBox() {
+    ComboBox<TransformationType> transformationComboBox = new ComboBox<>();
+    transformationComboBox.getItems().addAll(TransformationType.values());
+    transformationComboBox.setPromptText("Select Transformation");
+    return transformationComboBox;
+  }
+
+  private HBox createTransformationInputField(ComboBox<TransformationType> transformationComboBox) {
+    HBox transformationInputField = new HBox();
+    transformationInputField.setSpacing(10);
+
+    transformationComboBox.setOnAction(e -> updateTransformationFields(transformationComboBox, transformationInputField));
+    updateTransformationFields(transformationComboBox, transformationInputField);
+
+    return transformationInputField;
+  }
+
+  private TextField createTextField(String promptText) {
+    TextField textField = new TextField();
+    textField.setPromptText(promptText);
+    return textField;
+  }
+
+  private Button createSaveButton(ComboBox<TransformationType> transformationComboBox, HBox transformationInputField, TextField startVectorField, TextField endVectorField) {
+    Button saveButton = new Button("Save");
+    saveButton.getStyleClass().add("button");
+    saveButton.setOnAction(e -> {
+      // Implement the save logic here
+      String selectedTransformation = transformationComboBox.getValue().toString();
+      // Get vectors from vectorsBox
+      // Get start and end vectors
+      // Save the transformation using controller
+    });
+    return saveButton;
+  }
+
+  private Button createCancelButton(HBox transformationInputField, TextField startVectorField, TextField endVectorField) {
+    Button cancelButton = new Button("Cancel");
+    cancelButton.getStyleClass().add("button");
+    cancelButton.setOnAction(e -> {
+      transformationInputField.getChildren().clear();
+      startVectorField.clear();
+      endVectorField.clear();
+    });
+    return cancelButton;
+  }
+
+  private Button createAddFileButton() {
+    Button addFileButton = new Button("Add File");
+    addFileButton.getStyleClass().add("button");
+    addFileButton.setOnAction(e -> {
+      // Code to add file
+    });
+    return addFileButton;
+  }
+  private void updateTransformationFields(ComboBox<TransformationType> comboBox, HBox inputField) {
+    inputField.getChildren().clear();
+    if (comboBox.getValue() != null) {
+      switch (comboBox.getValue()) {
+        case JULIA:
+          inputField.getChildren().add(juliaTransformationTextField());
+          break;
+        case AFFINE:
+          inputField.getChildren().add(affineTransformationTextFields());
+          break;
+      }
+    }
+  }
+
+  public enum TransformationType {
+    JULIA,
+    AFFINE,
+  }
+
+  private VBox juliaTransformationTextField() {
+    VBox transformationVbox = new VBox();
+    TextField transformationField = new TextField();
+    transformationField.setPromptText("Transformation");
+    transformationVbox.getChildren().add(transformationField);
+    return transformationVbox;
+  }
+
+  private VBox affineTransformationTextFields() {
+    VBox transformationsBox = new VBox(5);
+    Button addTransformationButton = new Button("Add Transformation");
+
+    addTransformationButton.setOnAction(e -> {
+      HBox vectorBox = new HBox(5);
+      TextField vectorField = new TextField();
+      vectorField.setPromptText("Enter Affine Transformation");
+      vectorBox.getChildren().add(vectorField);
+      transformationsBox.getChildren().add(vectorBox);
+    });
+
+    transformationsBox.getChildren().add(addTransformationButton);
+    return transformationsBox;
+  }
+
+
+
+
+
+
+
+    /**
+     * Creates an input field for custom number of steps.
+     *
+     * @return The input field.
+     */
   private TextField createInputField() {
     TextField inputField = new TextField();
     inputField.setPromptText("Steps");
