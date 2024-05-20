dfa93cd5a12df0a6692d3fc2ac5db00fa8988514
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-05-20 12:26:44 +0200

    Docs: JavaDoc and fix checkstyle in StyledTextField and StyledButton

--- src/main/java/edu/ntnu/idatt2003/view/components/StyledButton.java
+++ src/main/java/edu/ntnu/idatt2003/view/components/StyledButton.java
@@ -10,22 +10,23 @@ import javafx.scene.control.Button;
  * specified event handler.
  * Goal: Create a styled button with a specified text, width, height and event handler.
  */
-public class StyledButton extends Button{
+public class StyledButton extends Button {
 
-    /**
-     * Creates a styled button with a specified text, width, height and event handler.
-     * The button has a style class "button" and the event handler is set to the
-     * specified event handler.
-     *
-     * @param text The text to display on the button.
-     * @param width The width of the button.
-     * @param height The height of the button.
-     * @param eventHandler The event handler to set on the button.
-     */
-    public StyledButton(String text, int width, int height, EventHandler<ActionEvent> eventHandler) {
-        super(text);
-        setPrefSize(width, height);
-        getStyleClass().add("button");
-        setOnAction(eventHandler);
-    }
+  /**
+   * Creates a styled button with a specified text, width, height and event handler.
+   * The button has a style class "button" and the event handler is set to the
+   * specified event handler.
+   *
+   * @param text         The text to display on the button.
+   * @param width        The width of the button.
+   * @param height       The height of the button.
+   * @param eventHandler The event handler to set on the button.
+   */
+  public StyledButton(String text, int width, int height,
+                      EventHandler<ActionEvent> eventHandler) {
+    super(text);
+    setPrefSize(width, height);
+    getStyleClass().add("button");
+    setOnAction(eventHandler);
+  }
 }
--- src/main/java/edu/ntnu/idatt2003/view/components/StyledTextField.java
+++ src/main/java/edu/ntnu/idatt2003/view/components/StyledTextField.java
@@ -2,7 +2,20 @@ package edu.ntnu.idatt2003.view.components;
 
 import javafx.scene.control.TextField;
 
+/**
+ * A styled text field with a specified prompt text, width and height.
+ * The text field has a style class "text-field".
+ * Goal: Create a styled text field with a specified prompt text, width and height.
+ */
 public class StyledTextField extends TextField {
+  /**
+   * Creates a styled text field with a specified prompt text, width and height.
+   * The text field has a style class "text-field".
+   *
+   * @param promptText The text to display in the text field when it is empty.
+   * @param width The preferred width of the text field.
+   * @param height The preferred height of the text field.
+   */
   public StyledTextField(String promptText, int width, int height) {
     super();
     this.setPromptText(promptText);
