c6d457dea2e9f96803ada2d2cac9cba3a87c881f
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2024-05-20 20:01:37 +0200

    Feat: Add new constructor to StyledButton where width is set to fill HBox

--- src/main/java/edu/ntnu/idatt2003/view/components/StyledButton.java
+++ src/main/java/edu/ntnu/idatt2003/view/components/StyledButton.java
@@ -3,6 +3,8 @@ package edu.ntnu.idatt2003.view.components;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 /**
  * A styled button with a specified tet width, height and event handler.
@@ -26,6 +28,24 @@ public class StyledButton extends Button {
                       EventHandler<ActionEvent> eventHandler) {
     super(text);
     setPrefSize(width, height);
+    getStyleClass().add("button");
+    setOnAction(eventHandler);
+  }
+
+  /**
+   * Creates a styled button with a specified text, height and event handler.
+   * The button has a style class "button" and the event handler is set to the
+   * specified event handler.
+   * The button's width is set to grow with the HBox.
+   *
+   * @param text The text to display on the button.
+   * @param height The height of the button.
+   * @param eventHandler The event handler to set on the button.
+   */
+  public StyledButton(String text, int height, EventHandler<ActionEvent> eventHandler) {
+    super(text);
+    HBox.setHgrow(this, Priority.ALWAYS);
+    setPrefHeight(height);
     getStyleClass().add("button");
     setOnAction(eventHandler);
   }
