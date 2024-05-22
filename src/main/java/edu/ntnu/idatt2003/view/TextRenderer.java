edfb29d7db8f1b9d89350bc00f18112bf29b05fc
Nicklas Tufteland <nicklapt@stud.ntnu.no>
2024-05-22 03:24:08 +0200

    Feat: Update createNewDescription method.

--- src/main/java/edu/ntnu/idatt2003/view/TextRenderer.java
+++ src/main/java/edu/ntnu/idatt2003/view/TextRenderer.java
@@ -13,15 +13,17 @@ public class TextRenderer {
   public static final String WRITE_FILE = "2";
   public static final String RUN_ITERATIONS = "3";
   public static final String SHOW_CANVAS = "4";
+  public static final String WRITE_NEW_DESCRIPTION = "5";
   public static final String EXIT = "5";
 
   public void showMenu() {
     System.out.println("Menu:");
     System.out.println("1. Read description from file");
-    System.out.println("2. Write description to file");
+    System.out.println("2. Write current description to file");
     System.out.println("3. Run ChaosGame a given number of steps");
     System.out.println("4. Show Canvas");
-    System.out.println("5. Exit");
+    System.out.println("5. Write New Description");
+    System.out.println("6. Exit");
   }
 
   public void enterPath() {
